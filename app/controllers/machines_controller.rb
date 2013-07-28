8bd87e5b359edd6f587d22cd742a40dc9aeed5bc
tr0ubl3 <mentor.1985@gmail.com>
2013-07-29 02:01:27 +0300

    adaugat manufacturers crud: list si new

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,7 +1,28 @@
 class MachinesController < ApplicationController
-	
+helper_method :manufacturers, :owners, :machine_manufacturer, :manufacturer_test?	
 	def list
-		
+		@machines = Machine.order("machines.id ASC")
 	end
 
+	def new
+		@machine = Machine.new
+	end
+
+	def create
+		@machine = Machine.new(params[:machine])
+		if @machine.save
+			flash[:notice] = "Successfully created machine."
+			redirect_to(:action => 'list')
+		else
+			render('new')
+		end
+	end
+
+	def manufacturers
+	 	@manufacturers ||= Manufacturer.all
+	end
+
+	def owners
+		@owners ||= MachineOwner.all
+	end
 end
