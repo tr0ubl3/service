9a05cb6b4fbb3116a14a19d7b07c9efab84c66ed
tr0ubl3 <mentor.1985@gmail.com>
2013-07-30 00:03:06 +0300

    adaugat crud pentru machine_manufacturer

--- app/controllers/machine_owners_controller.rb
+++ app/controllers/machine_owners_controller.rb
@@ -1,2 +1,52 @@
 class MachineOwnersController < ApplicationController
+	respond_to :html, :json
+
+	def list
+		@owners = MachineOwner.order("firms.id ASC")
+	end
+
+	def show
+		@owner = MachineOwner.find(params[:id])
+		@mo = @owner.machines.order("machines.id ASC")
+	end
+
+	def new
+		@owner = MachineOwner.new
+	end
+
+	def create
+		@owner = MachineOwner.new(params[:machine_owner])
+		if @owner.save
+			flash[:notice] = "Successfully created machine owner."
+			redirect_to(:action => 'list')
+		else
+			render('new')
+		end
+	end
+
+	def update
+		@owner = MachineOwner.find(params[:id])
+		respond_to do |format|
+			if @owner.update_attributes(params[:machine_owner])
+				flash[:notice] = "Successfully updated machine owner."
+				format.html { redirect_to(:action => 'show', :id => @owner.id) }
+			else
+				render('edit')
+			end
+	    end
+	end
+
+	def delete
+		@owner = MachineOwner.find(params[:id])
+	end
+
+	def destroy
+		owner = MachineOwner.find(params[:id]).destroy
+		flash[:notice] = "Machine owner permanently deleted !"
+		redirect_to(:action => 'list')
+	end
+
+	def edit
+		@owner = MachineOwner.find(params[:id])
+	end 
 end
--- app/models/machine_owner.rb
+++ app/models/machine_owner.rb
@@ -2,3 +2,4 @@ class MachineOwner < Firm
   attr_accessible :name, :country, :city, :address, :postal_code, :fax, :office_tel, :office_mail, :mobile
   has_many :machines
 end
+
