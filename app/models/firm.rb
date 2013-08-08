59ca7592b4032ade66c069acbf4e23fbd74b6428
tr0ubl3 <mentor.1985@gmail.com>
2013-08-08 13:23:23 +0300

    adaugat validari pt modelul firms

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,4 +1,28 @@
 class Firm < ActiveRecord::Base
-  attr_accessible :name, :country, :city, :address, :postal_code, :fax, :office_tel, :office_mail, :mobile
+  attr_accessible :name, :country, :city, :address, :postal_code, :fax, :office_tel, :office_mail, :mobile, :type
   has_many :machines
+  validates :name, :presence => true,
+  				   :length => { :within => 3..255 },
+  				   :uniqueness => true
+
+  validates :country, :presence => true,
+  				      :length => { :within => 3..255 }
+
+  validates :city, :presence => true,
+  				      :length => { :within => 3..255 }
+
+  validates :address, :presence => true,
+  				      :length => { :within => 3..255 }
+
+  validates :postal_code, :presence => true,
+  				      :length => { :within => 3..255 }
+
+  validates :office_tel, :presence => true,
+  				      :length => { :within => 3..255 }
+
+  validates :fax, :length => { :within => 3..255 }
+
+  validates :office_mail, :presence => true,
+  				          :length => { :within => 3..255 }
+  validates :mobile, :length => { :within => 8..255 }
 end
