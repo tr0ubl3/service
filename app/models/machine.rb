388a447e3b83dbdde5b18329d798b8fa7793bc08
tr0ubl3 <mentor.1985@gmail.com>
2013-08-08 13:34:12 +0300

    adaugat validari in modelul machines

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -3,4 +3,20 @@ class Machine < ActiveRecord::Base
   belongs_to :manufacturer
   belongs_to :machine_owner
   has_many :events
+#validari
+validates :display_name, :presence => true,
+  				   :length => { :within => 3..255 },
+  				   :uniqueness => true
+validates :machine_number, :presence => true,
+  				      :length => { :within => 3..255 }
+
+validates :machine_type, :presence => true,
+				      :length => { :within => 3..255 }
+
+validates :delivery_date, :presence => true,
+				      :length => { :within => 3..255 }
+
+validates :waranty_period, :presence => true,
+				      :length => { :within => 3..255 }
+
 end
