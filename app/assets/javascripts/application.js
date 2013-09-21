c6c0d2182529b8dde6190d417576b330f412a6c5
tr0ubl3 <mentor.1985@gmail.com>
2013-09-22 01:43:20 +0300

    adaugat validari jquery pt alarme: lungime, format, singularitate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,49 +33,83 @@ $(document).ready(function() {
 	// 	});
 // acjs - alarm code button java script
 
-(function() {
-	$('#alarm_code').on('keydown', doChk);
-	function doChk(e) {
-		if(e.keyCode == 13) {
-			doVal(e);
-		}
-	};
-	$('#acjs').on('click', doVal);
-function doVal(e) {
-	// acfi = alarm_code field id
-	acfi = $('#alarm_code');
+	var AlarmArray = [];
+	(function() {
 
-	// val = current value of field alarm_code
-	val = acfi.val();
-	$('#acdp').css('display', 'block');
-	e.preventDefault();
-	if(val === '') {
-		console.log('empty string detected');
-	}
-	else {
-		// acfi.closest('.control-group').append(val);
-		$('<p></p>', {
-			text: val,
-			class: 'alarm_code_add',
-			data: val
-		}).appendTo('#acdp');
-		acfi.val('');
-		}
-}
-})();
+		// insert data using enter key	
+		$('#alarm_code').on('keydown', doChk);
+		function doChk(e) {
+			if(e.keyCode == 13) {
+				doVal(e);
+			}
+		};
 
-(function() {
-	$('body').on('click', '.alarm_code_add', function () {
-		var t = $(this);
-		var text = t.text();
-		var confirmation = confirm("Are you sure you want to delete alarm:" + "\n" + text);
-		if (confirmation==true)
-		{
-			t.remove();
+		// insert data using button	
+		$('#acjs').on('click', doVal);
+
+	// function for inserting data	
+	function doVal(e) {
+		// acfi = alarm_code field id
+		acfi = $('#alarm_code');
+		// val = current value of field alarm_code
+		val = acfi.val();
+		var regex = /^\d{5,6}$/;
+		e.preventDefault();
+		acfi.closest('.control-group').removeClass('error');
+		if(val === '') {
+			console.log('empty string detected');
 		}
 
-	}
-	);
-})();
+		else if (!(regex.test(val)) ? true:false ) {
+			console.log('value '+ val +' is not a number or does not have the required length')
+		}
 
-});
+		else {
+			// acfi.closest('.control-group').append(val);
+			var ValVerify = true;
+			//verificare daca eroare introdusa nu a fost deja introdusa
+			$.each(AlarmArray, function() {
+				if(val == this) {
+					console.log('Alarm ' + this + ' already introduced');
+					ValVerify = false;
+					return;
+				}
+			});
+			console.log(ValVerify);
+			if(ValVerify != false) {
+				$('#acdp').css('display', 'block');
+				AlarmArray.push(val);
+				console.log('My alarms are '+ AlarmArray);
+				$('<p></p>', {
+					text: val,
+					class: 'alarm_code_add',
+					data: val
+				}).appendTo('#acdp');
+				acfi.val('');
+			}
+			else {
+				acfi.closest('.control-group').addClass('error');
+				}
+			}
+		}
+	})();
+
+	//anonymous function for deleting alarm after clicking on it
+	(function() {
+		$('body').on('click', '.alarm_code_add', function () {
+			var t = $(this);
+			var text = t.text();
+			var elem = AlarmArray.indexOf(text);
+			var confirmation = confirm("Are you sure you want to delete alarm:" + "\n" + text);
+			if (confirmation==true)
+			{
+				t.remove();
+				AlarmArray.splice(elem, 1);
+				console.log('array now is: ' + AlarmArray);
+			}
+
+		}
+		);
+	})();
+
+	});
