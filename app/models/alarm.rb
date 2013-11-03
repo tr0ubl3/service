d1f5eaecd8b83d6f7d9699a4c391436d723816cf
tr0ubl3 <mentor.1985@gmail.com>
2013-11-04 00:22:47 +0200

    modificare search query alarm to_i

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -3,7 +3,7 @@ class Alarm < ActiveRecord::Base
   has_and_belongs_to_many :events
   
   # t1 = test 1
-  scope :t1, lambda { |query| where(["number LIKE ?", "#{query}"]) }
+  scope :t1, lambda { |query| where(["number LIKE ?", "#{query}".to_i]) }
 
 	def self.seal(search_query)
 		get_data(search_query)
