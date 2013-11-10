5c203f64353bd4e319f3b77b754a99c8aba3c79a
tr0ubl3 <mentor.1985@gmail.com>
2013-11-10 20:39:13 +0200

    modificare general view pt listare masini in functie de owner, manufacturer, tip

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,13 +1,25 @@
 class GeneralController < ApplicationController
   before_filter :authenticate_user!
   def index
-	@machines = Machine.where(:machine_owner_id => current_user.machine_owner)
-	@manufacturers = []
-	@machines.each do |m|
-		@manufacturers << m.manufacturer.name
-	end
-	@manufacturers.uniq.each do |t|
-		@man_machines = Manufacturer.where(:name => t).first.machines
-	end
+	@manufacturer_ids = Machine.owner_manufacturer_ids(current_user.machine_owner)
+
+	# @manufacturer_ids.each do |id|
+	# 	@manufacturer_names = Machine.owner_manufacturer_name(id)
+	# 	@manufacturer_machine_types = Machine.manufacturer_machine_types(id)
+	# 	@manufacturer_machine_types.each do |type|
+	# 		@machines = Machine.owner_machines_by_manufacturer_and_type(params[current_user.machine_owner, id, type])
+	# 	end
+	# end
+	# @machines = Machine.where(:machine_owner_id => current_user.machine_owner)
+	# @machine_manufacturer_ids = @machines.select(:manufacturer_id).map(&:manufacturer_id).uniq
+	# @manufacturer_names = Manufacturer.where(:id => @machine_manufacturer_ids).select(:name).map(&:name)
+	# @machine_type = @machines.where().select(:machine_type).map(&:machine_type).map(&:upcase).uniq
+	# @manufacturers = []
+	# @machines.each do |m|
+	# 	@manufacturers << m.manufacturer.name
+	# end
+	# @manufacturers.uniq.each do |t|
+	# 	@man_machines = Manufacturer.where(:name => t).first.machines
+	# end
   end
 end
--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,20 +4,37 @@ class Machine < ActiveRecord::Base
   belongs_to :machine_owner
   has_many :events
   has_one :hour_counter
-validates :display_name, :presence => true,
-  				   :length => { :within => 3..255 },
-  				   :uniqueness => true
-validates :machine_number, :presence => true,
-  				      :length => { :within => 3..255 },
-  				      :uniqueness => true
+  validates :display_name, :presence => true,
+    				   :length => { :within => 3..255 },
+    				   :uniqueness => true
+  validates :machine_number, :presence => true,
+    				      :length => { :within => 3..255 },
+    				      :uniqueness => true
 
-validates :machine_type, :presence => true,
-				      :length => { :within => 3..255 }
+  validates :machine_type, :presence => true,
+  				      :length => { :within => 3..255 }
 
-validates :delivery_date, :presence => true,
-				      :length => { :within => 3..255 }
+  validates :delivery_date, :presence => true,
+  				      :length => { :within => 3..255 }
 
-validates :waranty_period, :presence => true,
-				      :length => { :within => 3..255 }
+  validates :waranty_period, :presence => true,
+  				      :length => { :within => 3..255 }
+  
 
+  def self.owner_manufacturer_name(owner)
+    # Manufacturer.where(:id => owner_manufacturer_ids(owner)).select(:name).map(&:name)
+    Manufacturer.find(owner).name
+  end
+
+  def self.manufacturer_machine_types(manufacturer)
+    Machine.where(:manufacturer_id => manufacturer).select(:machine_type).map(&:machine_type).map(&:upcase).uniq
+  end
+
+  def self.owner_machines_by_manufacturer_and_type(owner, manufacturer, type)
+    Machine.where(:machine_owner_id => owner, :manufacturer_id => manufacturer, :machine_type => type)
+  end
+
+  def self.owner_manufacturer_ids(owner_id)
+    Machine.where(:machine_owner_id => owner_id).select(:manufacturer_id).map(&:manufacturer_id).uniq
+  end
 end
