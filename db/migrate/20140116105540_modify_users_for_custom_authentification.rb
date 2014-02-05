6d7be1031ea1b8c6f6ea139dfb2529e1ea6a598a
tr0ubl3 <mentor.1985@gmail.com>
2014-02-05 13:02:52 +0200

    fix migration for heroku

--- db/migrate/20140116105540_modify_users_for_custom_authentification.rb
+++ db/migrate/20140116105540_modify_users_for_custom_authentification.rb
@@ -4,7 +4,7 @@ class ModifyUsersForCustomAuthentification < ActiveRecord::Migration
     remove_index :users, :reset_password_token
   	remove_columns(:users, :nick, :encrypted_password, :reset_password_token,
   				 	:reset_password_sent_at, :remember_created_at, :sign_in_count,
-  				 	:current_sign_in_at, :last_sign_in_at, :current_sign_in_at,
+            :last_sign_in_at, :current_sign_in_at,
   				 	:current_sign_in_ip, :last_sign_in_ip)
   end
 
