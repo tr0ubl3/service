2b336df10006c177ede1c4ec464f17b05c13496c
tr0ubl3 <mentor.1985@gmail.com>
2014-03-06 16:04:57 +0200

    modificare query_state pt compatibilitate heroku pg

--- app/models/service_event_state.rb
+++ app/models/service_event_state.rb
@@ -2,7 +2,7 @@ class ServiceEventState < ActiveRecord::Base
   attr_accessible :state
   belongs_to :service_event
 
-  def self.with_last_state(state)
-  	order("id desc").group(self.column_names.collect {|c| "service_event_states.#{c}"}.join(",")).having(state: state)
+  def self.with_last_state
+  	order("id desc")
   end
 end
