9bc58e3a02539fff2086b2d16429ab2257d4abe8
tr0ubl3 <mentor.1985@gmail.com>
2014-03-13 21:45:52 +0200

    adaugat scalare imagine la upload pt o marime mai mica

--- app/uploaders/event_evaluation_uploader.rb
+++ app/uploaders/event_evaluation_uploader.rb
@@ -36,11 +36,13 @@ class EventEvaluationUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :scale => [1280, 960]
   #
-  # def scale(width, height)
-  #   # do something
-  # end
+  def scale(width, height)
+    manipulate! do |img|
+      img.resize_to_fit(width)
+    end
+  end
 
   # Create different versions of your uploaded files:
   version :thumb do
