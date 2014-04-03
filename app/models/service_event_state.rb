c3c6072f40150a5e70f21ae25d5f09f12eecdde6
tr0ubl3 <mentor.1985@gmail.com>
2014-04-03 23:32:07 +0300

    fixed#2 query db for pending 'state' event

--- app/models/service_event_state.rb
+++ app/models/service_event_state.rb
@@ -3,6 +3,6 @@ class ServiceEventState < ActiveRecord::Base
   belongs_to :service_event
 
   def self.with_last_state(state)
-  	where("service_event_states.id IN (SELECT MAX(id) FROM service_event_states)").where(:state => state)
+  	where("service_event_states.id IN (SELECT MAX(id) FROM service_event_states GROUP BY service_event_id)").where(:state => state)
   end
 end
