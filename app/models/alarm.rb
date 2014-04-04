c52acc9c727ac15c95428d1e3774ea8417cd4751
tr0ubl3 <mentor.1985@gmail.com>
2014-04-04 11:15:35 +0300

    sters metode nefolosite

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -3,20 +3,4 @@ class Alarm < ActiveRecord::Base
   has_and_belongs_to_many :service_events
   
   scope :search, lambda { |query| where(["number = ?", "#{query}".to_i]) }
-
-	def self.seal(search_query)
-		get_data(search_query)
-		@g
-	end
-
-private
-	def self.get_data(eval)
-		e = t1(eval)
-		@g = nil 
-		if e.length == 1
-			e.each do |e|
-				@g = e.text
-			end
-		end
-	end
 end
